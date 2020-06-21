49651b90d339122a0071bf7b75fc5e98d77da7e6
Pushpak <pushpakmhatre77@gmail.com>
2020-06-20 22:05:46 -0500

    Updates

---
+++ precache-manifest.8ef0a2c225bccd42e0bf0a0b8def8499.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07b0f3c34f328445a792c547262b0361",
+    "url": "/testapp/index.html"
+  },
+  {
+    "revision": "ca58d5cf16bc3697c7e8",
+    "url": "/testapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c69c894df23546079913",
+    "url": "/testapp/static/js/2.92b1f7a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testapp/static/js/2.92b1f7a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca58d5cf16bc3697c7e8",
+    "url": "/testapp/static/js/main.6eab7984.chunk.js"
+  },
+  {
+    "revision": "e9180ab0e541f873aa90",
+    "url": "/testapp/static/js/runtime-main.3a92724d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
